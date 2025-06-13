969cb04d2115726b6ec12e9e9d0c61387c20310e
Niamh Brockbank <niamh.brockbank@imgarena.com>
2025-06-13 14:27:15 +0100

    Add accordion functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ function App() {
       <h2>Why work with us?</h2>
 
       <Accordion className="accordion">
-        <AccordionItem className="accordion-item" title="We've got loads of experience">
+        <AccordionItem 
+          id="experience"
+          className="accordion-item" 
+          title="We've got loads of experience">
             <article>
               <p>You cannot go wrong with us</p>
               <p>We've had ages of time to go wrong and perfect before your vacation.</p>
             </article>
         </AccordionItem>
-        <AccordionItem className="accordion-item" title="We work with local guides">
+        <AccordionItem 
+          id="local-guides"
+          className="accordion-item" 
+          title="We work with local guides">
           <article>
             <p>We are in the field keeping up to date with the hot spots.</p>
           </article>
--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,39 @@
+import { createContext, useContext, useState } from "react"
+
+const AccordionContext = createContext()
+
+export function useAccordionContext() {
+    const ctx = useContext(AccordionContext)
+
+    if (!ctx){
+        throw new Error('To use this requires the <Accordion> wrapper component.')
+    }
+
+    return ctx
+}
+
 export default function Accordion({children, className}){
-    return <ul className={className}>
-        {children}
-    </ul>
+    const [openItemId, setOpenItemId] = useState()
+
+    function openItem(id) {
+        setOpenItemId(id)
+    }
+
+    function closeItem() {
+        setOpenItemId(null)
+    }
+
+    const contextValue = {
+        openItemId,
+        openItem,
+        closeItem
+    }
+
+    return (
+    <AccordionContext.Provider value={contextValue}>
+        <ul className={className}>
+            {children}
+        </ul>
+    </AccordionContext.Provider>
+    )
 }
