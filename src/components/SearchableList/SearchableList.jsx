e60f16f5560ea44e2de1d2b4b709da85d5c8234c
Niamh Brockbank <niamh.brockbank@imgarena.com>
2025-06-13 15:58:48 +0100

    Add most simple search logic

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function SearchableList({items}) {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    function handleChange(event){
+        setSearchTerm(event.target.value)
+    }
+
+    const searchResults = items.filter(item => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()))
+
     return <div className="searchable-list">
-        <input type="search" placeholder="Search"/>
+        <input type="search" placeholder="Search" onChange={handleChange}/>
         <ul>
-            {items.map((item, i) => 
+            {searchResults.map((item, i) => 
                 <li key={`${i}-${item}`}>{item.toString()}</li>
             )}
         </ul>
