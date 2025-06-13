fcdcb7236b6a5e97b554a7e5a416a877f2833b0d
Niamh Brockbank <niamh.brockbank@imgarena.com>
2025-06-13 15:54:23 +0100

    Add the components for a searchable list component

---
+++ src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,10 @@
+export default function SearchableList({items}) {
+    return <div className="searchable-list">
+        <input type="search" placeholder="Search"/>
+        <ul>
+            {items.map((item, i) => 
+                <li key={`${i}-${item}`}>{item.toString()}</li>
+            )}
+        </ul>
+    </div>
+}
