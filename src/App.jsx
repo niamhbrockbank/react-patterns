3e47a3a9f0f31de4ebd2faac5cb49402b5598a1d
Niamh Brockbank <niamh.brockbank@imgarena.com>
2025-06-13 15:20:07 +0100

    Abstract into more configurable Accordion sub-components

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,24 @@ function App() {
       <Accordion className="accordion">
         <Accordion.Item 
           id="experience"
-          className="accordion-item" 
-          title="We've got loads of experience">
-            <article>
-              <p>You cannot go wrong with us</p>
-              <p>We've had ages of time to go wrong and perfect before your vacation.</p>
-            </article>
+          className="accordion-item" >
+            <Accordion.Title id="experience" className="accordion-item-title">We've got loads of experience</Accordion.Title>
+            <Accordion.Content id="experience" className="accordion-item-content">
+              <article>
+                <p>You cannot go wrong with us</p>
+                <p>We've had ages of time to go wrong and perfect before your vacation.</p>
+              </article>
+            </Accordion.Content>
         </Accordion.Item>
         <Accordion.Item 
           id="local-guides"
-          className="accordion-item" 
-          title="We work with local guides">
-          <article>
-            <p>We are in the field keeping up to date with the hot spots.</p>
-          </article>
+          className="accordion-item" >
+            <Accordion.Title id="local-guides" className="accordion-item-title">We work with local guides</Accordion.Title>
+            <Accordion.Content id="local-guides" className="accordion-item-content">
+              <article>
+                <p>We are in the field keeping up to date with the hot spots.</p>
+              </article>
+            </Accordion.Content>
         </Accordion.Item>
       </Accordion>
     </section>
